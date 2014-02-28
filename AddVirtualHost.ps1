a542ae5561901ff71894e24703b0a587e743642c
Mariusz Wojcik <mariusz.wojcik@hotmail.co.uk>
2014-02-28 23:02:10 +0000

    Added Add-RabbitMQVirtualHost cmdlet

---
+++ AddVirtualHost.ps1
@@ -0,0 +1,56 @@
+﻿function Add-RabbitMQVirtualHost
+{
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact="Low")]
+    Param
+    (
+        # Name of RabbitMQ Virtual Host.
+        [parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [Alias("vh", "VirtualHost")]
+        [string[]]$Name = "",
+
+        # Name of the computer hosting RabbitMQ server. Defalut value is localhost.
+        [parameter(ValueFromPipelineByPropertyName=$true)]
+        [Alias("HostName", "hn", "cn")]
+        [string]$ComputerName = $defaultComputerName,
+        
+        # UserName to use when logging to RabbitMq server. Default value is guest.
+        [string]$UserName = $defaultUserName,
+
+        # Password to use when logging to RabbitMq server. Default value is guest.
+        [string]$Password = $defaultPassword
+    )
+
+    Begin
+    {
+        Add-Type -AssemblyName System.Web
+        Add-Type -AssemblyName System.Net
+         
+        $cred = GetRabbitMqCredentials $UserName $Password
+        $cnt = 0
+    }
+    Process
+    {
+        if (-not $pscmdlet.ShouldProcess("server: $ComputerName", "Add vhost(s): $(NamesToString $Name '(all)')")) {
+            foreach ($qn in $Name) 
+            { 
+                Write "Creating new Virtual Host $qn on server $ComputerName" 
+                $cnt++
+            }
+
+            return
+        }
+
+        foreach($n in $Name)
+        {
+            $url = "http://$([System.Web.HttpUtility]::UrlEncode($ComputerName)):15672/api/vhosts/$([System.Web.HttpUtility]::UrlEncode($n))"
+            $result = Invoke-RestMethod $url -Credential $cred -ErrorAction Continue -Method Put -ContentType "application/json"
+
+            Write-Verbose "Created Virtual Host $n on server $ComputerName"
+            $cnt++
+        }
+    }
+    End
+    {
+        Write-Verbose "Created $cnt Virtual Host(s)."
+    }
+}
---
+++ Tests/AddVirtualHost.Tests.ps1
@@ -0,0 +1,88 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$here\..\AddVirtualHost.ps1"
+
+$server = "192.168.232.129"
+
+function AssertAreEqual($actual, $expected) {
+
+    if ($actual -is [System.Array]) {
+        if ($expected -isnot [System.Array]) { throw "Expected {$expected} to be an array, but it is not." }
+
+        if ($actual.Length -ne $expected.Length)
+        { 
+            $al = $actual.Length
+            $el = $expected.Length
+            throw "Expected $al elements but were $el"
+        }
+
+        for ($i = 0; $i -lt $actual.Length; $i++)
+        {
+            $a = $actual[$i]
+            $e = $expected[$i]
+            if ($a -ne $e) 
+            { 
+                throw "Expected element at position $i to be {$e} but was {$a}" 
+            }
+        }
+    }
+}
+
+Describe -Tags "Example" "Add-RabbitMQVirtualHost" {
+    It "should create new Virtual Host" {
+
+        Add-RabbitMQVirtualHost -ComputerName $server "vh3"
+        
+        $actual = Get-RabbitMQVirtualHost -ComputerName $server "vh3" | select -ExpandProperty name 
+        
+        $actual | Should Be "vh3"
+    }
+
+    It "should do nothing when VirtualHost already exists" {
+
+        Add-RabbitMQVirtualHost -ComputerName $server "vh3"
+        Add-RabbitMQVirtualHost -ComputerName $server "vh3"
+
+        $actual = Get-RabbitMQVirtualHost -ComputerName $server "vh3" | select -ExpandProperty name 
+        
+        $actual | Should Be "vh3"
+    }
+
+    It "should create many Virtual Hosts" {
+
+        Add-RabbitMQVirtualHost -ComputerName $server "vh3", "vh4"
+
+        $actual = Get-RabbitMQVirtualHost -ComputerName $server "vh3", "vh4" | select -ExpandProperty name 
+
+        $expected = $("vh3", "vh4")
+
+        AssertAreEqual $actual $expected
+    }
+
+    It "should get VirtualHost to be created from the pipe" {
+
+        $("vh3", "vh4") | Add-RabbitMQVirtualHost -ComputerName $server
+        
+        $actual = $("vh3", "vh4") | Get-RabbitMQVirtualHost | select -ExpandProperty name 
+
+        $expected = $("vh3", "vh4")
+
+        AssertAreEqual $actual $expected
+    }
+
+    It "should get VirtualHost with ComputerName to be created from the pipe" {
+
+        $pipe = $(
+            New-Object -TypeName psobject -Prop @{"ComputerName" = $server; "Name" = "vh3" }
+            New-Object -TypeName psobject -Prop @{"ComputerName" = $server; "Name" = "vh4" }
+        )
+
+        $pipe | Add-RabbitMQVirtualHost
+
+        $actual = $pipe | Get-RabbitMQVirtualHost | select -ExpandProperty name 
+
+        $expected = $("vh1", "vh2")
+
+        AssertAreEqual $actual $expected
+    }
+}
+
